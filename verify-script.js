9a7c695b036dab14783e08cbedb4dd1b95888223
kacijcox <145575906+kacijcox@users.noreply.github.com>
2025-05-04 00:01:23 +0000

    fix inline script

---
+++ verify-script.js
@@ -0,0 +1,5 @@
+// Script to verify loading of all required modules
+console.log("Script loading verification:");
+console.log("- SecurityUtils loaded:", !!window.SecurityUtils);
+console.log("- SessionData loaded:", !!window.SessionData);
+console.log("- SecurityAlerts loaded:", !!window.SecurityAlerts);
